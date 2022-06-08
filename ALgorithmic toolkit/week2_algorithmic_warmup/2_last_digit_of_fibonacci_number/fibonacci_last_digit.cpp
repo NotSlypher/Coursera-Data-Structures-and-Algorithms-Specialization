7eb42d0c02a2c8da3a26417b75d4dbc3fdb9b5c3
NotSlypher <86952522+NotSlypher@users.noreply.github.com>
2022-06-09 00:32:14 +0530

    week 2

--- ALgorithmic toolkit/week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.cpp
+++ ALgorithmic toolkit/week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
+#include <vector>
 
 int get_fibonacci_last_digit_naive(int n) {
-    if (n <= 1)
-        return n;
-
-    int previous = 0;
-    int current  = 1;
-
-    for (int i = 0; i < n - 1; ++i) {
-        int tmp_previous = previous;
-        previous = current;
-        current = tmp_previous + current;
+    if (n == 0)
+        return 0;
+    if (n == 1)
+        return 1;
+    std::vector<ll> a(n);
+    for (long long i = 0; i < n; ++i)
+    {
+        if (i < 2)
+        {
+            a[0] = 1ll;
+            a[1] = 1ll;
+        }
+        else
+        {
+            a[i] = (a[i - 1ll] + a[i - 2ll]) % 10;
+        }
     }
-
-    return current % 10;
+    return a[n - 1ll] % 10;
 }
 
 int main() {
--- ALgorithmic toolkit/week2_algorithmic_warmup/5_fibonacci_number_again/fibonacci_huge.cpp
+++ ALgorithmic toolkit/week2_algorithmic_warmup/5_fibonacci_number_again/fibonacci_huge.cpp
@@ -1,23 +1,29 @@
 #include <iostream>
-
-long long get_fibonacci_huge_naive(long long n, long long m) {
-    if (n <= 1)
-        return n;
-
-    long long previous = 0;
-    long long current  = 1;
-
-    for (long long i = 0; i < n - 1; ++i) {
-        long long tmp_previous = previous;
-        previous = current;
-        current = tmp_previous + current;
+#include <vector>
+#define ll long long
+long long get_fibonacci_huge_fast(long long n, long long m) {
+    if (n == 0)
+        return 0;
+    if (n == 1)
+        return 1;
+    std::vector<ll> a(n);
+    for (long long i = 0; i < n; ++i)
+    {
+        if (i < 2)
+        {
+            a[0] = 1ll;
+            a[1] = 1ll;
+        }
+        else
+        {
+            a[i] = (a[i - 1ll] + a[i - 2ll]) % m;
+        }
     }
-
-    return current % m;
+    return a[n - 1ll] % m;
 }
 
 int main() {
     long long n, m;
     std::cin >> n >> m;
-    std::cout << get_fibonacci_huge_naive(n, m) << '\n';
+    std::cout << get_fibonacci_huge_fast(n, m) << '\n';
 }
