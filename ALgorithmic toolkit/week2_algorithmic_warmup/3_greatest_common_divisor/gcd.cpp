c9412f70cb45f439b5eb9fea34ecb8f687abffc1
NotSlypher <86952522+NotSlypher@users.noreply.github.com>
2022-06-09 13:24:13 +0530

    .

--- ALgorithmic toolkit/week2_algorithmic_warmup/3_greatest_common_divisor/gcd.cpp
+++ ALgorithmic toolkit/week2_algorithmic_warmup/3_greatest_common_divisor/gcd.cpp
@@ -12,9 +12,17 @@ int gcd_naive(int a, int b) {
   return current_gcd;
 }
 
+int gcd_euclidean(int a, int b)
+{
+    if (b == 0)
+        return a;
+    else
+        return gcd_euclidean(b, a % b);
+}
 int main() {
   int a, b;
   std::cin >> a >> b;
-  std::cout << gcd_naive(a, b) << std::endl;
+  // std::cout << gcd_naive(a, b) << std::endl;
+  std::cout << gcd_euclidean(a, b) << std::endl;
   return 0;
 }
--- ALgorithmic toolkit/week2_algorithmic_warmup/4_least_common_multiple/lcm.cpp
+++ ALgorithmic toolkit/week2_algorithmic_warmup/4_least_common_multiple/lcm.cpp
@@ -8,9 +8,19 @@ long long lcm_naive(int a, int b) {
   return (long long) a * b;
 }
 
+long long gcd_euclidean(int a, int b)
+{
+    if (b == 0)
+        return a;
+    else
+        return gcd_euclidean(b, a % b);
+}
+
 int main() {
-  int a, b;
-  std::cin >> a >> b;
-  std::cout << lcm_naive(a, b) << std::endl;
+  long long c, d, gcd, a, b;
+  std::cin >> c >> d;
+  a = c, b = d;
+  gcd = gcd_euclidean(a, b);
+  std::cout << c * d / gcd << std::endl;
   return 0;
 }
