06c0c232fefb0c71db24685fcad6cdf8075554b3
NotSlypher <86952522+NotSlypher@users.noreply.github.com>
2022-07-31 00:47:14 +0530

    algo week 5

--- ALgorithmic toolkit/week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.cpp
+++ ALgorithmic toolkit/week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.cpp
@@ -1,23 +1,39 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-
+#include <cmath>
+#define ld long double
 using std::vector;
 
 vector<int> optimal_sequence(int n) {
-  std::vector<int> sequence;
-  while (n >= 1) {
-    sequence.push_back(n);
-    if (n % 3 == 0) {
-      n /= 3;
-    } else if (n % 2 == 0) {
-      n /= 2;
-    } else {
-      n = n - 1;
-    }
+  std::vector<int> sequences;
+  vector<int> array(n + 1);
+  int a1, m2, m3;
+  for (int i = 2; i <= n; ++i)
+  {
+	  a1 = m2 = m3 = 10e9;
+	  if (i - 1 >= 0) a1 = 1 + array[i - 1];
+	  if (i % 2 == 0 && i / 2 >= 0) m2 = 1 + array[i/2];
+	  if (i % 3 == 0 && i / 3 >= 0) m3 = 1 + array[i/3];
+	  array[i] = std::min(a1, std::min(m2, m3));
   }
-  reverse(sequence.begin(), sequence.end());
-  return sequence;
+  while (n > 1) {
+	  if (n % 3 == 0 and array.at(n / 3) + 1 == array.at(n)) {
+		  sequences.push_back(n);
+		  n /= 3;
+	  }
+	  else if (n % 2 == 0 and array.at(n / 2) + 1 == array.at(n)) {
+		  sequences.push_back(n);
+		  n /= 2;
+	  }
+	  else if (array.at(n - 1) + 1 == array.at(n)) {
+		  sequences.push_back(n);
+		  n -= 1;
+	  }
+  }
+  sequences.push_back(1);
+  reverse(sequences.begin(), sequences.end());
+  return sequences;
 }
 
 int main() {
--- ALgorithmic toolkit/week5_dynamic_programming1/3_edit_distance/edit_distance.cpp
+++ ALgorithmic toolkit/week5_dynamic_programming1/3_edit_distance/edit_distance.cpp
@@ -1,11 +1,33 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using std::string;
 
 int edit_distance(const string &str1, const string &str2) {
   //write your code here
-  return 0;
+	int n = str1.length(), m = str2.length();
+	std::vector<std::vector<int>> dp(n + 1);
+  for (int i = 0; i <= n; ++i)
+	  for (int j = 0; j <= m; ++j)
+		  dp[i].push_back(j);
+
+  for (int i = 0; i < n + 1; ++i)
+	  dp[i][0] = i;
+  for (int i = 0; i < m + 1; ++i)
+	  dp[0][i] = i;
+
+  for (int i = 1; i < n + 1; ++i)
+  {
+	  for (int j = 1; j < m + 1; ++j)
+	  {
+		  if (str1[i - 1] == str2[j - 1])
+			  dp[i][j] = dp[i - 1][j - 1];
+		  else
+			  dp[i][j] = std::min(dp[i - 1][j] + 1,std::min(1 + dp[i][j - 1], 1 + dp[i - 1][j - 1]));
+	  }
+  }
+  return dp[n][m];
 }
 
 int main() {
